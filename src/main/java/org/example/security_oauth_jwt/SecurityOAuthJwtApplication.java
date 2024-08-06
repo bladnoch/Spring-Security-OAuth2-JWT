b7b99f1349588b428773bc6615509c6f1d2a58f6
DK <dongwook5478@gmail.com>
2024-08-06 10:18:39 +0900

    FIRST COMMIT

---
+++ src/main/java/org/example/security_oauth_jwt/SecurityOAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package org.example.security_oauth_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOAuthJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOAuthJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/security_oauth_jwt/SecurityOAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.security_oauth_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOAuthJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
