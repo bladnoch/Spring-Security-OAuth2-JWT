256e37d5b2f82eaa94523326a2cad2256c6a1b04
DK <dongwook5478@gmail.com>
2024-08-09 23:47:10 +0900

    fix: 구글 로그인이 안되던 문제 해결 -> GoogleResponse.java에 getProviderId()에 오토가 있었음

--- src/main/java/org/example/security_oauth_jwt/jwt/JWTFilter.java
+++ src/main/java/org/example/security_oauth_jwt/jwt/JWTFilter.java
@@ -25,6 +25,20 @@ public class JWTFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestUri = request.getRequestURI();
+
+        if (requestUri.matches("^\\/login(?:\\/.*)?$")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+        if (requestUri.matches("^\\/oauth2(?:\\/.*)?$")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
         String authorization = null;
         Cookie[] cookies = request.getCookies();
 
